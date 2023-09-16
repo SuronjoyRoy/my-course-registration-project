ba0ea98f6bc36670620fc542156dafe4a96ef8cb
SuronjoyRoy <suronjoyroy21@gmail.com>
2023-09-16 17:18:54 +0600

    card section total add and total hour added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,17 +1,21 @@
 /* eslint-disable react/prop-types */
-const Cart = ({selectLanguages}) => {
-    console.log(selectLanguages)
+const Cart = ({selectLanguage, remaining, totalHr}) => {
+    // console.log(selectLanguages)
+    console.log(remaining);
     return (
         <div className="">
-            <h3 className="pl-3 text-lg font-bold text-blue-600 pt-3">Credit Hour Remaining 7 hr</h3>
+            <h3 className="pl-3 text-lg font-bold text-blue-600 pt-3">Credit Hour Remaining {remaining} hr</h3>
             <h3 className="pl-3 text-2xl font-bold pb-5 mt-6 border-t-2">Course Name</h3>
             {
-                selectLanguages.map(language => (
+                selectLanguage.map(language => (
                 <li className="pl-3" key={language.id}>{language.title}</li>
                 ))
                 
             }
-            <h3 className="pl-3 pt-[80px] pb-14 text-xl ">Total Credit Hour : </h3>
+            <div className="border-t-1 mt-5"></div>
+            <hr />
+            <h3 className="pl-3 pt-[20px] font-medium pb-1 text-xl ">Total Credit Hour :{totalHr} </h3>
+            <h3 className="pl-3 pt-[20px] font-medium pb-14 text-xl ">Total Price : </h3>
         </div>
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,83 +1,92 @@
-
-import { useState } from "react";
-import { useEffect } from "react";
-import { FaDollarSign} from 'react-icons/fa';
+import { useState, useEffect } from "react";
+import { FaDollarSign } from "react-icons/fa";
 import Cart from "../Cart/Cart";
 
-
 const Home = () => {
-    
-    const [allLanguage, setAllLanguage] = useState([])
-    const [selectLanguages, setSelectLanguages] = useState([])
-    useEffect(() =>{
-        fetch('data.json')
-        .then(response => response.json())
-        .then(data => setAllLanguage(data))
-    },[])
-    const handelSelect = (language) =>{
-        const isExist = selectLanguages.find(item => item.id ==language.id)
-        let count = language.reading_time
-        if(isExist){
-            return(
-                alert('Just One Click')
-                
-            )
-        }
-        else{
-            selectLanguages.forEach((item) => {
-                count = count  + item.reading_time
-            })
-            console.log(count)
-            setSelectLanguages([...selectLanguages, language])
+  const [allLanguage, setAllLanguage] = useState([]);
+  const [selectLanguage, setSelectLanguages] = useState([]);
+  const [remaining, setRemaining] = useState(20); 
+  const [totalHr, setTotalHr] = useState(0);
 
-        }
-    
+  useEffect(() => {
+    fetch("data.json")
+      .then((response) => response.json())
+      .then((data) => setAllLanguage(data));
+  }, []);
+
+  const handleSelect = (language) => {
+    const isExist = selectLanguage.find((item) => item.id === language.id);
+
+    if (isExist) {
+      return alert("You have already added");
+    } else {
+      let count = parseFloat(language.reading_time);
+
+      if (totalHr + count > 20) {
+        return alert("You cannot add more than 20");
+      } else {
+        setTotalHr(totalHr + count);
+        setRemaining(20 - totalHr); 
+        setSelectLanguages([...selectLanguage, language]);
+      }
     }
+  };
 
-    return (
-        <div className="container mt-4">
-            <div className="home-container flex">
-                <div className="card-container w-auto  gap-4 grid md:grid-cols-3 grid-cols-1 " >
-                {
-                    
-                    allLanguage.map((language) =>(
-
-                <div key={language.id} className="card">
-                    
-                <div className="card w-80 h-[400px] bg-base-100 shadow-xl">
+  return (
+    <div className="container mt-4">
+      <div className="home-container flex">
+        <div className="card-container w-auto  gap-4 grid md:grid-cols-3 grid-cols-1 ">
+          {allLanguage.map((language) => (
+            <div key={language.id} className="card">
+              <div className="card w-80 h-[400px] bg-base-100 shadow-xl">
                 <figure className="px-4 pt-4">
-                <img src={language.cover}  />
+                  <img src={language.cover} alt="Language Cover" />
                 </figure>
                 <div className="card-body">
-                <h2 className="card-title text-black  font-semibold">{language.title}</h2>
-                <p><small className="text-gray-600">{language.text}</small></p>
-                <div className="flex justify-between">
-                
-                <div className="flex">
-                <span><FaDollarSign className="mt-1"></FaDollarSign></span> 
-                <p >Price: {language.price}</p>
+                  <h2 className="card-title text-black  font-semibold">
+                    {language.title}
+                  </h2>
+                  <p>
+                    <small className="text-gray-600">{language.text}</small>
+                  </p>
+                  <div className="flex justify-between">
+                    <div className="flex">
+                      <span>
+                        <FaDollarSign className="mt-1" />
+                      </span>
+                      <p>Price: {language.price}</p>
+                    </div>
+                    <div className="flex">
+                      <a href="">
+                        <img src="Frame.png" alt="" className="mr-2" />
+                      </a>
+                      <p>Credit: {language.reading_time} hr</p>
+                    </div>
+                  </div>
+                  <div className="card-actions justify-center mt-2">
+                    <button
+                      onClick={() => handleSelect(language)}
+                      className="bg-[#2f80ed] w-full rounded-lg p-2 text-white"
+                    >
+                      Select
+                    </button>
+                  </div>
                 </div>
-                <div className="flex">
-                    <a href=""><img src="Frame.png" alt="" className="mr-2" /></a>
-                <p>Credit: {language.reading_time} hr</p>
-                </div>
-                </div>
-                <div className="card-actions justify-center mt-2">
-                <button onClick={() =>handelSelect(language)} className="bg-yellow-900 px-10 p-2 text-white">Select</button>
-                </div>
-                </div>
-                </div>
-                </div>
-                    ))
-                }
-                </div>
-                <div className=" w-[275px] h-max bg-base-100 shadow-md ml-5">
-                    <Cart selectLanguages = {selectLanguages}></Cart>
-
-                </div>
+              </div>
             </div>
+          ))}
         </div>
-    );
+        <div className="w-[275px] h-max bg-base-100 shadow-md ml-5">
+        
+          <Cart
+            selectLanguage={selectLanguage}
+            remaining={remaining}
+            totalHr={totalHr}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
